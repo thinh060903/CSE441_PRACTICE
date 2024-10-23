19cd76314a57e96784242273912974b4f4da61d7
Cù Thịnh <thinh060903@gmail.com>
2024-10-24 00:45:45 +0700

    Update ex20

---
+++ Ex20/app/src/main/java/com/example/ex20/MyAsyncTask.java
@@ -0,0 +1,49 @@
+package com.example.ex20;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MyAsyncTask extends AsyncTask<Void, Integer, Void> {
+    Activity contextCha;
+
+    public MyAsyncTask(Activity ctx) {
+        contextCha = ctx;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Toast.makeText(contextCha, "onPreExecute!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        for (int i = 0; i <= 100; i++) {
+            SystemClock.sleep(100);
+            publishProgress(i);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        ProgressBar paCha = (ProgressBar) contextCha.findViewById(R.id.progressBar1);
+        int giatri = values[0];
+        paCha.setProgress(giatri);
+        TextView txtmsg = (TextView) contextCha.findViewById(R.id.textView1);
+        txtmsg.setText(giatri + "%");
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        Toast.makeText(contextCha, "Update xong rồi!", Toast.LENGTH_LONG).show();
+    }
+}
+
+
